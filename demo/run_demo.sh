b71d5993ed4c174d1a0db6bfba9f6cc643df2e0e
Antonio Ulloa <antonio.ulloa@nih.gov>
2025-05-09 11:46:28 -0400

    minor changes such as removing py from script module...

---
+++ demo/run_demo.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# This script runs the LSNM model with a specific configuration.
+# It uses a model file, a weights list, and a script for the DMS configuration.
+../lsnm -m model.txt -w weightslist.txt -s script_DMS_198_seconds
